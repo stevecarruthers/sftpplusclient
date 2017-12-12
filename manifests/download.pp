728df5e10e9115cd39cfab9edc1b8d3d3ffca173
root <root@agent1.tickbox>
2017-12-12 19:58:00 +0000

    download.pp and init.pp

---
+++ manifests/download.pp
@@ -0,0 +1,20 @@
+class sftpplusclient::download (
+   String $proatria_dl   = 'http://download.sftpplus.com/client',
+   String $major_version = '1',
+   String $minor_version = '5',
+   String $sftpplus_tar  = 'sftpplus-client-rhel6-x86-1.5.64.tar.gz',
+   String $download_location = '/tmp',
+   Integer $timeout = 60,
+   Boolean $verbose = true,
+)
+{
+
+  include ::wget
+
+  ::wget::fetch { "${proatria_dl}/${major_version}/${minor_version}/${sftpplus_tar}":
+    destination => "${download_location}/$(sftpplus_tar}",
+    timeout     => $timeout,
+    verbose     => $verbose,
+  }
+
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -44,5 +44,6 @@
 #
 class sftpplusclient {
 
+ class {'::sftpplusclient::doiwnload':}
 
 }
